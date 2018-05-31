4c7c3743265c8094292f7b8c8df821097d916abe
Ruslan Ibragimov <ruslan@ibragimov.by>
2018-05-31 08:42:54 +0300

    Move Application class to expected by shade plugin location: io.bootique.cayenne.demo package.

--- src/main/java/io/bootique/cayenne/demo/Application.java
+++ src/main/java/io/bootique/cayenne/demo/Application.java
@@ -1,17 +1,20 @@
+package io.bootique.cayenne.demo;
+
 import com.google.inject.Binder;
 import com.google.inject.Module;
 import io.bootique.BQCoreModule;
 import io.bootique.Bootique;
 import io.bootique.cayenne.CayenneModule;
-import io.bootique.cayenne.demo.InsertDataCommand;
-import io.bootique.cayenne.demo.PostPersistListener;
-import io.bootique.cayenne.demo.SelectDataCommand;
 import org.apache.cayenne.configuration.Constants;
 import org.apache.cayenne.configuration.server.ServerModule;
 
 public class Application implements Module {
     public static final void main(String[] args) {
-        Bootique.app(args).module(Application.class).autoLoadModules().exec().exit();
+        Bootique.app(args)
+                .module(Application.class)
+                .autoLoadModules()
+                .exec()
+                .exit();
     }
 
     @Override
@@ -21,12 +24,11 @@ public class Application implements Module {
                 .addCommand(SelectDataCommand.class);
 
         CayenneModule.extend(binder)
-                //non-default Cayenne project name requires an explicit declaration
+                // non-default Cayenne project name requires an explicit declaration
                 .addProject("cayenne-myproject.xml")
                 .addModule(cayenneBinder -> {
-                    //override Cayenne module settings, e.g. this one
+                    // override Cayenne module settings, e.g. this one
                     ServerModule.contributeProperties(cayenneBinder).put(Constants.JDBC_MAX_QUEUE_WAIT_TIME, "0");
                 }).addListener(PostPersistListener.class);
-
     }
 }
